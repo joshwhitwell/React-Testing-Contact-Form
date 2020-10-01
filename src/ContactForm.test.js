2cfd027927a75c86c14703ea7bf45e419679e247
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-01 15:46:27 -0400

    added input can type tests to contactform.test.js

---
+++ src/ContactForm.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import * as rlt from '@testing-library/react'
+import ContactForm from './components/ContactForm'
+
+describe('ContactForm Tests', () => {
+    test("renders ContactForm without crashing", () => {
+      rlt.render(<ContactForm />);
+    });
+})
+
+describe('can type in input fields', () => {
+    test('can type in input files', () => {
+        //Render Form
+        rlt.render(<ContactForm />);
+        
+        //Grab Input Elements
+        const fnInput = rlt.screen.getByLabelText(/First Name*/i)
+        const lnInput = rlt.screen.getByLabelText(/Last Name*/i)
+        const eInput = rlt.screen.getByLabelText(/Email*/i)
+        const mInput = rlt.screen.getByLabelText(/Message/i)
+        const button = rlt.screen.getByRole('button', {type: /submit/i})
+
+        //Fire Input Events
+        rlt.fireEvent.change(fnInput, {target: {name: 'firstName', value: 'Josh'}})
+        rlt.fireEvent.change(lnInput, {target: {name: 'lastName', value: 'Whitwell'}})
+        rlt.fireEvent.change(eInput, {target: {name: 'email', value: 'josh.whitwell@gmail.com'}})
+        rlt.fireEvent.change(mInput, {target: {name: 'message', value: 'nothing much to say'}})
+
+        //Assertions
+        expect(fnInput.value === 'Josh').toBeTruthy()
+        expect(lnInput.value === 'Whitwell').toBeTruthy()
+        expect(eInput.value === 'josh.whitwell@gmail.com').toBeTruthy()
+        expect(mInput.value === 'nothing much to say').toBeTruthy()
+    })
+})
+
+    
